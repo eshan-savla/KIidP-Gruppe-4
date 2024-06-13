a015943cfbc38c8db3a3c08f4c7cf02d760da6e0
mathias31415 <mathias31415@gmx.de>
2024-06-13 16:52:32 +0200

    added grconvnet_mdaf_single with single mdaf in bottleneck

--- inference/models/__init__.py
+++ inference/models/__init__.py
@@ -23,6 +23,10 @@ def get_network(network_name):
         return GenerativeResnet
      # GR-Confnet with RFB in bottleneck and multi-directional attention fusion
     elif network_name == 'grconvnet3_rfb_mdaf_single':
+        from .grconvnet3_mdaf_single import GenerativeResnet
+        return GenerativeResnet
+    # GR-Confnet with RFB in bottleneck and multi-directional attention fusion
+    elif network_name == 'grconvnet3__mdaf_single':
         from .grconvnet3_rfb_mdaf_single import GenerativeResnet
         return GenerativeResnet
         # GR-Confnet with RFB  in bottleneck and multi-directional attention fusions including concatenation of shallow and deep features in upsampling blocks
