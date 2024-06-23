e3a2044385b57048b8e8f4ff3dc2ea9318857539
mathias31415 <148789048+mathias31415@users.noreply.github.com>
2024-06-23 15:32:52 +0200

    changed grconvnet3_mdaf_single.py to grconvnet3_1mdaf.py

--- inference/models/__init__.py
+++ inference/models/__init__.py
@@ -27,8 +27,8 @@ def get_network(network_name):
         from .grconvnet3_rfb import GenerativeResnet
         return GenerativeResnet
      # GR-Confnet with RFB in bottleneck and multi-directional attention fusion
-    elif network_name == 'grconvnet3_mdaf_single':
-        from .grconvnet3_mdaf_single import GenerativeResnet
+    elif network_name == 'grconvnet3_1mdaf':
+        from .grconvnet3_1mdaf import GenerativeResnet
         return GenerativeResnet
     # GR-Confnet with RFB in bottleneck and multi-directional attention fusion
     elif network_name == 'grconvnet3_rfb_mdaf_single':
