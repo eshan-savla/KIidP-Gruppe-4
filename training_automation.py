b82fe5c6d681f0ed3a36aae8238a7e982d9bc891
maudetroll <mauricedroll@gmail.com>
2024-06-25 13:00:20 +0000

    vonHand: Commit zu: training_grovnnet_with_Parameter

--- training_automation.py
+++ training_automation.py
@@ -40,7 +40,6 @@ def execute_command(command):
 
 # Liste der Python-Befehle
 python_commands = [                                                                        
-    "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet --epochs 50 --description training_grovnnet0_no_channelsize",
     "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet2 --epochs 50 --description training_grovnnet_with_Parameter",
     "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet2_MaxPooling --epochs 50 --description training_grovnnet_with_MaxPooling",
     "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet2_1ResBlock --epochs 50 --description training_grovnnet_with_1ResBlock",
@@ -48,30 +47,41 @@ python_commands = [
     "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet3_1rfb --epochs 50 --description training_grovnnet3_with_1RFB",
     "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet3_1rfb_1mdaf_5residual --epochs 50 --description training_grovnnet3_with_5RFB",
     "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet3_1rfb_1mdaf_1residual --epochs 50 --description training_cornell_1rfb_1mdfa_1residual",
-
-
+    "python train_network.py --dataset cornell --dataset-path utils/data/cornell --network grconvnet3_with_MaxPooling.py --epochs 50 --description training_grovnnet3_with_MaxPooling",
 ]
 
 commit_messages = [
-    "Commit zu: training_grovnnet0_no_channelsize",
     "Commit zu: training_grovnnet_with_Parameter ",
     "Commit zu: training_grovnnet_with_MaxPooling ",
     "Commit zu: training_grovnnet_with_1ResBlock ",
     "Commit zu: training_grovnnet3_with_1MDAF ",
     "Commit zu: training_grovnnet3_with_1RFB ",
-   
+    "Commit zu: training_grovnnet3_with_5RFB ",
+    "Commit zu: training_cornell_1rfb_1mdfa_1residual ",
+    "Commit zu: training_grovnnet3_with_MaxPooling ",
 
     # Weitere Commit-Nachrichten können hier hinzugefügt werden
     # "Automated commit: another_script"
 
 ]
 
+i =0
 # Iteriere durch die Liste der Python-Befehle und führe jeden aus
 for python_command in python_commands:
-    for commit_message in commit_messages:
-        execute_command(python_command)
-        run_git_commands(commit_message)
-        print(f"Executed command: Befehl ausgeführt")
-        continue
+    
+    print(f"Execute command: {python_command}")
+    execute_command(python_command)
+    print(f"ExecuteD command: {python_command}")
+
+    print(f"Started command: " + commit_messages[i])
+    run_git_commands(commit_messages)
+    print(f"Executed command: " + commit_messages[i])
+    i= i+1
+    
+    
+    #for commit_message in commit_messages:
+    #    run_git_commands(commit_message)
+    #    print(f"Executed command: " + commit_message)
+    #continue
 
 
